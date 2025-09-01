684436faed7366bae5198ae203280683600e61f9
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-09-01 12:18:58 +0300

    feat: crypto hashing

--- crypto/src/index.ts
+++ crypto/src/index.ts
@@ -1,13 +1,27 @@
-import { poseidonHashBN254 } from "garaga";
+import { poseidonHashBN254, initSync, init as initGaraga } from "garaga";
 
-export function txSecretHash(key: string, to: string): bigint {
+export async function txSecretHash(key: string, to: string): Promise<bigint> {
   // should use bn245 poseidon hash as used in circom
   // hash key and recipient to generate tx secret
   if (key && to) {
     // both should be non zero
-    return poseidonHashBN254(BigInt(key), BigInt(to));
+    return hash(BigInt(key), BigInt(to));
   } else {
     // both should be non zero
     return BigInt(0);
+  }
+}
+
+export async function hash(a: bigint, b: bigint): Promise<bigint> {
+  try {
+    return poseidonHashBN254(a, b);
+  } catch (error) {
+    await initGaraga();
+    try {
+      return poseidonHashBN254(a, b);
+    } catch (error) {
+      console.error("Failed hashing with garaga.");
+      throw error;
+    }
   }
 }
