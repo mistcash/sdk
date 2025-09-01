69dd82556ccd40f6800db0a5c3ef67c5981ffd5c
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-09-01 16:06:26 +0300

    feat(core): getContract and devVal fix

--- core/src/utils.ts
+++ core/src/utils.ts
@@ -1,3 +1,16 @@
+import { AccountInterface, Contract, ProviderInterface } from 'starknet';
+import { CHAMBER_ABI, CHAMBER_ADDR_MAINNET } from '@mistcash/config';
+
+export function getContract(provider?: ProviderInterface | AccountInterface): Contract {
+	return new Contract(
+		CHAMBER_ABI,
+		CHAMBER_ADDR_MAINNET,
+		provider
+	)
+}
+
 export const devStr = (val: string) => devVal(val, '') as string;
 
-export const devVal = <T,>(val: T, deflt: T | undefined = undefined) => localStorage.getItem('devVals') ? val : deflt;
+export const devVal = <T,>(val: T, deflt: T | undefined = undefined) => {
+	return typeof window !== 'undefined' && window.localStorage.getItem('devVals') ? val : deflt
+};
