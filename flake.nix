8b1d6fc059fe407ea16bb657ff96d7ca75ad8db2
Mugen <pierre.semanne@gmail.com>
2026-01-10 15:40:54 +0100

    chore: boilerplate setup

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs-20_x
+            nodejs_20
             pnpm
           ];
         };
